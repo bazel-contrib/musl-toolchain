2c211a8c17de11c6db396bec2ddd2ec3782dfa21
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-06-25 15:56:04 +0200

    Try forcing -static

--- build.sh
+++ build.sh
@@ -63,14 +63,19 @@ if [[ "Linux" == "$(uname)" ]]; then
   # Clean previous build artifacts but keep downloaded sources and stage1 output
   make clean
   rm -rf build/
+  # Create a wrapper around stage1 GCC that always links statically.
+  cat > "${working_directory}/output_stage1/bin/${TARGET}-gcc-static" <<EOF
+#!/bin/sh
+exec ${working_directory}/output_stage1/bin/${TARGET}-gcc "\$@" -static
+EOF
   # Point CC to the stage1 GCC and set LDFLAGS for static linking.
   # We also need to tell musl-cross-make to build a "native" compiler.
   # The `HOST` variable in litecross/Makefile is used for this.
   # We'll also introduce a new variable `STATIC_HOST_COMPILER=y` to signal
   # that we want the host components (i.e., gcc itself) to be static.
-  TARGET="${TARGET}" HOST="${PLATFORM}" CC="${working_directory}/output_stage1/bin/${TARGET}-gcc" CC_FOR_BUILD=gcc LDFLAGS="-static" \
+  TARGET="${TARGET}" HOST="${PLATFORM}" CC="${working_directory}/output_stage1/bin/${TARGET}-gcc-static" \
       make MUSL_VER="${MUSL_VERSION}" GNU_SITE="https://mirror.netcologne.de/gnu/"
-  TARGET="${TARGET}" HOST="${PLATFORM}" CC="${working_directory}/output_stage1/bin/${TARGET}-gcc" CC_FOR_BUILD=gcc LDFLAGS="-static" \
+  TARGET="${TARGET}" HOST="${PLATFORM}" CC="${working_directory}/output_stage1/bin/${TARGET}-gcc-static" \
       make MUSL_VER="${MUSL_VERSION}" GNU_SITE="https://mirror.netcologne.de/gnu/" install
 else
   # Standard single-stage build for non-Linux platforms (macOS)
