8b6ce0225dad018f84aae6562ee014a24c022899
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-06-27 11:58:50 +0200

    t

--- build.sh
+++ build.sh
@@ -74,7 +74,12 @@ EOF
   # We'll also introduce a new variable `STATIC_HOST_COMPILER=y` to signal
   # that we want the host components (i.e., gcc itself) to be static.
   TARGET="${TARGET}" HOST="${PLATFORM}" CC="${working_directory}/output_stage1/bin/${TARGET}-gcc-static" \
-      make MUSL_VER="${MUSL_VERSION}" GNU_SITE="https://mirror.netcologne.de/gnu/" || { cat config.log; exit 1; }
+      make MUSL_VER="${MUSL_VERSION}" GNU_SITE="https://mirror.netcologne.de/gnu/" || {
+        # print contents of config.log in all subdirectories
+        echo "Build failed, printing config.log files:"
+        find . -name config.log -exec echo "Contents of {}:" \; -exec cat {} \; -exec echo \;
+        exit 1
+    }
   TARGET="${TARGET}" HOST="${PLATFORM}" CC="${working_directory}/output_stage1/bin/${TARGET}-gcc-static" \
       make MUSL_VER="${MUSL_VERSION}" GNU_SITE="https://mirror.netcologne.de/gnu/" install
 else
